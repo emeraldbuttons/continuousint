79c83009f11ad1105c6d6c6a861ea2a5ae182224
Felix Brucker <bruckerl@oregonstate.edu>
2021-03-11 09:11:03 -0800

    Edited test formatting

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,10 @@
 import calculator
 
+
 class TestCalculator:
     def test_add(self):
         assert 10 == calculator.add(4, 6)
+
+        
     def test_subtract(self):
         assert -2 == calculator.subtract(7, 9)
